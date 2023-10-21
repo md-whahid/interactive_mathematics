b606ea04342bfef76bf9a3b0b49ce9f97d373e5c
md-whahid <mdislamwhahid@gmail.com>
2023-10-21 23:05:02 +0600

    first commit 10/21/23

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarButton = document.querySelector("[data-sidebar-button]");
+const sidebarLink = document.querySelector("[data-sidebar-link]");
+const sidebarLinkText = document.querySelector("[data-sidebar-link-text]");
+
+sidebarButton.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+});
+
+sidebarLinkText.addEventListener("click", () => {
+  sidebarLink.classList.toggle("active");
+});
